0eb68862dc738782aee05ff57444acf77d0d0c07
curly08 <mattynoakes08@gmail.com>
2022-01-12 12:26:21 -0500

    Add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { in: 0..40 }
+  validates :description, presence: true, length: { in: 0..400 }
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ },
+                    numericality: { greater_than: 0, less_than: 10_000 }
 end
