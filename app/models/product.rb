5ad97fb1786cf2ccf93dcd1cf478bb75654cf20f
curly08 <mattynoakes08@gmail.com>
2022-01-12 21:44:27 -0500

    Add #to_csv

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,14 @@ class Product < ApplicationRecord
   validates :description, presence: true, length: { in: 0..400 }
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ },
                     numericality: { greater_than: 0, less_than: 10_000 }
+
+  def self.to_csv
+    CSV.generate(headers: true) do |csv|
+      csv << column_names
+
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
